f868c822a5a58c76856b40d3c91781ea4274a405
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-05-03 19:01:32 +0600

    added jwt and verify token

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
-
-console.log(process.env.DB_PASS)
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj1q2ho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,16 +27,116 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares 
+const logger = (req, res, next) => {
+console.log("log info", req.method, req.url);
+next();
+}
+
+const verifyToken = (req, res, next) =>{
+  const token = req.cookies?.token;
+  if(!token){
+    return res.status(401).send({message : 'unauthorized access'})
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message : 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const serviceCollection = client.db("carDoctorDB").collection("services")
+const bookingCollection = client.db("carDoctorDB").collection("bookings")
+
+// jwt Token api
+app.post('/jwt', logger, async(req, res)=>{
+  const user = req.body;
+  console.log(user)
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , { expiresIn: '1h' })
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: false,
+  })
+  .send({success: true});
+})
+
+// service related api
+ app.get('/services', async(req, res)=>{
+    const cursor = serviceCollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+ });
+
+ app.get('/services/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const options = {
+        // Include only i want specific data 1 mean i want 0 mean do not want
+        projection: {title: 1, price: 1, service_id: 1, img: 1 },
+      };
+    const result = await serviceCollection.findOne(query, options);
+    res.send(result)
+ });
+
+//  booking
+app.get('/bookings', logger, verifyToken, async(req, res)=>{
+  console.log("token", req.cookies?.token)
+ 
+  if(req.query.email !== req.user.email){
+    return res.status(403).send({message: 'forbidden access'})
+  }
+  let query = {};
+  if(req.query.email){
+    query = { email: req.query.email}
+  }
+  const result = await bookingCollection.find(query).toArray();
+  res.send(result);
+})
+
+app.post('/bookings', logger, async(req, res)=>{
+  const booking = req.body;
+  console.log(booking)
+  const result = await bookingCollection.insertOne(booking);
+  res.send(result)
+});
+
+app.patch('/bookings/:id', async(req, res) =>{
+  const updatedBookings = req.body;
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id)};
+  const updateDoc = {
+    $set: {
+      status: updatedBookings.status
+    }
+  }
+  const result = await bookingCollection.updateOne(query, updateDoc);
+  res.send(result);
+
+})
+
+app.delete('/bookings/:id', async(req, res)=>{
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id)};
+  const result = await bookingCollection.deleteOne(query);
+  res.send(result);
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
